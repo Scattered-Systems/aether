0e6eeae02d78066ebb8524664e19991fc25610f2
FL03 <jo3mccain@icloud.com>
2022-05-24 11:23:28 -0500

    Commit
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- aether/src/settings.rs
+++ aether/src/settings.rs
@@ -1,26 +1,33 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Database {
+    pub name: String,
+    pub uri: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Project {
-    name: String,
-    slug: String,
+    pub name: String,
+    pub slug: String,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Provider {
-    endpoint: String,
-    public_key: String,
-    secret_key: String,
+    pub endpoint: String,
+    pub public_key: String,
+    pub secret_key: String,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Server {
-    host: [u8; 4],
-    port: u16,
+    pub host: [u8; 4],
+    pub port: u16,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Settings {
+    pub databases: Box<Database>,
     pub project: Project,
     pub providers: Box<Provider>,
     pub server: Server,
