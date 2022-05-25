f54b99e77b734724f5a0289c7f511c82ef538e40
FL03 <jo3mccain@icloud.com>
2022-05-25 08:10:55 -0500

    Commit
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- .space.kts
+++ .space.kts
@@ -1,13 +1,13 @@
-job("Build and Push: aether") {
+job("Build and Push: RustBox") {
     docker {
         build {
-            context = "./aether"
-            file = "./aether/Dockerfile"
-            args["HTTP_PROXY"] = "http://0.0.0.0:8888"
+            context = "."
+            file = "./Dockerfile"
+            args["HTTP_PROXY"] = "http://0.0.0.0:8080"
             labels["vendor"] = "scattered-systems"
         }
 
-        push("scattered-systems.registry.jetbrains.space/p/scsys/containers/aether") {
+        push("scattered-systems.registry.jetbrains.space/p/personal/containers/rustbox") {
             tags("0.0.\$JB_SPACE_EXECUTION_NUMBER", "latest")
         }
     }
