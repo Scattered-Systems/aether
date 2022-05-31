f82821d4cbc6cedd968fd64014f4b3c783d69387
FL03 <jo3mccain@icloud.com>
2022-05-31 11:52:47 -0500

    Commit: Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- code/crates/aether/src/blockchain.rs
+++ code/crates/aether/src/blockchain.rs
@@ -1,54 +1,25 @@
-use acme::data::{Clock, Identity};
+use acme::primitives::{Clock, Identity};
 use chrono::Local;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Hash, PartialEq, Serialize)]
 pub struct Block {
-    pub id: Identity,
-    pub created: Clock,
+    pub id: ObjectId,
     pub hash: String,
-    // Block Hash
     pub previous: String,
-    // Previous Hash
     pub nonce: String,
+    pub timestamp: Clock,
 }
 
 impl Block {
     pub fn new(hash: String, previous: String, nonce: String) -> Self {
         Self {
-            id: Identity::new(),
-            created: Local::now().into(),
+            id: ObjectId::new(),
             hash,
             previous,
             nonce,
+            timestamp: Local::now().into(),
         }
     }
 }
 
-pub trait Chain {
-    fn genesis(&self) -> Box<Block>;
-}
-
-enum OperatingStates {
-    Initializing,
-    Initialized,
-    Terminated,
-}
-
-pub struct Interface {
-    pub blocks: Box<Block>,
-}
-
-impl Interface {
-    fn new(blocks: Box<Block>) -> Self {
-        Self {
-            blocks,
-        }
-    }
-}
-
-impl Chain for Interface {
-    fn genesis(&self) -> Box<Block> {
-        todo!()
-    }
-}
